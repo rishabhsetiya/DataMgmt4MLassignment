bf6b2d44b3ecbf28ce8aaf4cf57980f09a2aebbc
Rishabh Setiya <rishabhsetiya7@gmail.com>
2025-03-13 02:04:23 +0530

    added validation stage

---
+++ src/prepare.py
@@ -0,0 +1,128 @@
+import pyodbc
+import os
+import requests
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+def impute_missing_values(df):
+    """
+    Imputes missing values in a DataFrame:
+    - Numerical columns: Replaced with mean
+    - Categorical columns: Replaced with mode (most frequent value)
+    """
+    for column in df.columns:
+        if df[column].dtype == "object":  # Check if column is categorical
+            df[column].fillna(df[column].mode()[0], inplace=True)  # Fill with mode
+        else:
+            df[column].fillna(df[column].mean(), inplace=True)  # Fill with mean
+    return df
+
+
+# Function for processing
+def data_processing(source, destination):
+    print ("PROCESSING THE FILE ", source)
+    df = pd.read_csv(source)
+
+    print(df.head(2))
+    print("Shape of the dataset ", df.shape)
+    print("Data types of the columns ", df.dtypes)
+    print("Information of the dataset ", df.info())
+
+    # Convert 'TotalCharges' to numeric (errors='coerce' converts non-numeric to NaN)
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+    print ("Datatype of Total Charges :", df['TotalCharges'].dtype)
+
+    df = impute_missing_values(df)
+    df = df.drop_duplicates()
+
+    # To see the correlation of the numeric fields
+    df.corr(numeric_only=True)
+
+    # Dropping 'customerID' as it's not needed
+    df.drop(columns=['customerID'], inplace=True)
+
+    # Creating Features
+    # Convert tenure into categories
+    def tenure_category(tenure):
+        if tenure <= 12:
+            return "Short-term"
+        elif tenure <= 48:
+            return "Medium-term"
+        else:
+            return "Long-term"
+    df['TenureCategory'] = df['tenure'].apply(tenure_category)
+
+    # Create interaction feature: Total Spend
+    df['TotalSpend'] = df['tenure'] * df['MonthlyCharges']
+
+    # Create binary feature: Has Internet
+    df['HasInternet'] = df['InternetService'].apply(lambda x: 0 if x == "No" else 1)
+
+    # Count of services opted
+    df['NumServices'] = df[['OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
+                            'TechSupport', 'StreamingTV', 'StreamingMovies']].apply(lambda x: sum(x == 'Yes'), axis=1)
+
+
+    # Standardize numerical columns
+    num_cols = ['tenure', 'MonthlyCharges', 'TotalCharges', 'TotalSpend']
+    scaler = StandardScaler()
+    df[num_cols] = scaler.fit_transform(df[num_cols])
+
+    #Encoding categorical variables
+    cat_cols = ['gender', 'Partner', 'Dependents', 'PhoneService', 'MultipleLines',
+                'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
+                'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract',
+                'PaperlessBilling', 'PaymentMethod', 'Churn', 'TenureCategory']
+
+    label_encoders = {}
+    for col in cat_cols:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+        label_encoders[col] = le
+
+    print(df.head(2))
+
+    #Perform EDA
+    # Plot Churn distribution
+    plt.figure(figsize=(6, 4))
+    sns.countplot(x=df["Churn"], palette="viridis")
+    plt.title("Churn Distribution")
+    plt.xlabel("Churn (0 = No, 1 = Yes)")
+    plt.ylabel("Count")
+    plt.show()
+
+    # Plot numerical feature distributions
+    df[num_cols].hist(figsize=(12, 5), bins=30, layout=(1, 4), color='purple', edgecolor='black')
+    plt.suptitle("Distribution of Standardized Numerical Features")
+    plt.show()
+
+    # Boxplot to detect outliers
+    plt.figure(figsize=(12, 5))
+    sns.boxplot(data=df[num_cols], palette="coolwarm")
+    plt.title("Boxplot of Standardized Numerical Features (Outliers Detection)")
+    plt.xticks(rotation=45)
+    plt.show()
+
+    # Save cleaned data
+    df.to_csv(destination, index=False)
+
+    print("Data Preprocessing, EDA and feature engineering Completed and Saved.")
+    print("\n")
+
+def main():
+    # File paths
+    SYNTHETIC_CSV_PATH = sys.argv[1]
+    KAGGLE_CSV_PATH = sys.argv[2]
+    params = yaml.safe_load(open("params.yaml"))["prepare"]
+    DATA_PATH = params["data_path"]
+    KAGGLE_CSV_PATH_PROCESSED = os.path.join(DATA_PATH, "static_data.csv")
+    SYNTHETIC_CSV_PATH_PROCESSED = os.path.join(DATA_PATH, "api_data.csv")
+    data_processing(KAGGLE_CSV_PATH, KAGGLE_CSV_PATH_PROCESSED)
+    data_processing(SYNTHETIC_CSV_PATH, SYNTHETIC_CSV_PATH_PROCESSED)
+
+if __name__ == "__main__":
+    main()
---
+++ src/store.py
@@ -0,0 +1,85 @@
+import pyodbc
+import os
+import requests
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import yaml
+import sys
+
+
+def saving_to_sql (source_file):
+    params = yaml.safe_load(open("params.yaml"))["store"]
+    server = params["server"]
+    username = params["username"]
+    password = params["password"]
+    driver = params["driver"]
+
+    # Create connection string
+    conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE=master;UID={username};PWD={password}', autocommit = True)
+
+    # Create a cursor
+    cursor = conn.cursor()
+
+    db_name = 'TELCO_CHURN_DB';
+
+    query = f"SELECT name FROM sys.databases WHERE name = '{db_name}'"
+    cursor.execute(query)
+    row = cursor.fetchone()
+    if row:
+        print(f"Database '{db_name}' already exists.")
+    else:
+        print(f"Database '{db_name}' does not exist. Creating now...")
+
+        # Create the database
+        cursor.execute(f"CREATE DATABASE {db_name}")
+        conn.commit()
+        print(f"Database '{db_name}' created successfully.")
+
+    conn_str = f"mssql+pyodbc://{username}:{password}@{server}/{db_name}?driver=ODBC+Driver+17+for+SQL+Server"
+    engine = create_engine(conn_str)
+    conn = engine.connect()
+    print("Connected successfully!")
+
+    df = pd.read_csv(source_file)
+    df["tenure"] = df["tenure"].apply(lambda x: round(x, 5))
+    df["MonthlyCharges"] = df["MonthlyCharges"].apply(lambda x: round(x, 5))
+    df["TotalCharges"] = df["TotalCharges"].apply(lambda x: round(x, 5))
+    df["TotalSpend"] = df["TotalSpend"].apply(lambda x: round(x, 5))
+    table_name = 'telco_churn_table'
+    query = f"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{table_name}'"
+
+    cursor.execute(query)
+    row = cursor.fetchone()
+
+    if row:
+        print(f"Table '{table_name}' exists. Appending the data", source_file)
+        df.to_sql(table_name, con=engine, if_exists="append", index=False)
+        print("Data written to sql server successfully")
+    else:
+        print(f"Table '{table_name}' does not exist. Creating Table")
+        create_table_query = """
+            CREATE TABLE telco_churn_table (
+            gender int, SeniorCitizen int, Partner int, Dependents int, tenure float, PhoneService int, MultipleLines int, InternetService int,
+            OnlineSecurity int, OnlineBackup int, DeviceProtection int, TechSupport int, StreamingTV int, StreamingMovies int, Contract int,
+            PaperlessBilling int, PaymentMethod int, MonthlyCharges float, TotalCharges float, Churn int, TenureCategory int,
+            TotalSpend float, HasInternet int, NumServices int
+            )
+           """
+        cursor.execute(create_table_query)
+        #conn.commit()
+        print ("Table Created successfully")
+        df.to_sql(table_name, con=engine, if_exists="append", index=False)
+
+    cursor.close()
+    conn.close()
+
+def main():
+    saving_to_sql(sys.argv[1])
+    saving_to_sql(sys.argv[2])
+
+if __name__ == "__main__":
+    main()
